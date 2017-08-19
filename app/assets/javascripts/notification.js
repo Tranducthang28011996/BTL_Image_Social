59c5a80b7414c869ed4c06e8eb7a26841f6fc10c
Tuyen Vu Duc <tuyenvuduc@yahoo.com>
2017-08-19 12:17:57 +0700

    notification follow

---
+++ app/assets/javascripts/notification.js
@@ -0,0 +1,31 @@
+$(function(){
+  $('body').on('click', '.notify-like', function(){
+    $('.block-content-notification').hide();
+    $('.block-loading').show();
+    $('.notification-block').toggle();
+
+      $.ajax({
+        url: '/notification',
+        method: 'GET',
+        dateType: 'JSON',
+        success: function(data){
+          console.log(data);
+          $('.block-loading').fadeOut(500);
+          $('.block-content-notification').html(data.notifications)
+          $('.block-content-notification').fadeIn(500);
+        }
+      });
+  });
+
+  // $(document).click(function(e){
+  //   if ($(e.target).is('.notification-block')) {
+  //   }else
+  //   if ($(e.target).is('.notify-block')) {
+  //   }else
+  //   if ($(e.target).is('.notify-like')) {
+  //   }else {
+  //     $('.notification-block').hide();
+  //   }
+  // });
+
+});
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,10 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
   def get_notification
-    @notification = current_user.passive_notifications
+    @notifications = current_user.passive_notifications
+    render json: {
+      notifications: render_to_string(partial: "notifications/notification",
+              locals: {notifications: @notifications})
+    }
   end
 end
